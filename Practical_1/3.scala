18810727607c60ffedf2ab21fdf2ba865061ba5c
AthharPro <athharasif2002@gmail.com>
2024-06-24 20:24:26 +0530

    test commit

--- Practical_1/3.scala
+++ Practical_1/3.scala
@@ -3,12 +3,12 @@
 object object3{
    def volumeOfSphere(r: Double): Double = {
       var pi = math.Pi
-      var vol = 4/3 * pi * r*r*r
+      var vol = 4.0/3.0 * pi * r*r*r
       vol
    }
 
    def main(args: Array[String]): Unit = {
-      var radius = 5
+      var radius = 5.0
       var volume = volumeOfSphere(radius)
       println(f"Volume of a Sphere with radius $radius%.2f is $volume%.2f")
    }
