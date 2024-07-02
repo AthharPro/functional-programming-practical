b36549afcc612aff87f37d53812e31940f93b025
AthharPro <athharasif2002@gmail.com>
2024-07-02 09:49:03 +0530

    practical 03

--- Practical_3/3.scala
+++ Practical_3/3.scala
@@ -3,7 +3,7 @@ import scala.io.StdIn.readLine
 object prac3_3 {
 
   def meanValue(num1: Int, num2: Int): Float = {
-      val mean = (num1.toFloat + num2.toFloat) / 3.0
+      val mean = (num1.toFloat + num2.toFloat) / 2.0
       val roundVal = math.round(mean * 100.0) / 100.0
       return roundVal.toFloat
   }
